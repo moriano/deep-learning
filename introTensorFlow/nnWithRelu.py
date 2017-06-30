b4136221c65ae8d5f4626cb84aa1782aa4e8e143
moriano <moriano@gmail.com>
2017-07-01 08:48:15 +1000

    Added a few examples with tensorflow, including a CNN

--- introTensorFlow/nnWithRelu.py
+++ introTensorFlow/nnWithRelu.py
@@ -1,7 +1,9 @@
 # Solution is available in the other "solution.py" tab
 import tensorflow as tf
 
-output = None
+# Solution is available in the other "solution.py" tab
+import tensorflow as tf
+
 hidden_layer_weights = [
     [0.1, 0.2, 0.4],
     [0.4, 0.6, 0.6],
@@ -21,12 +23,11 @@ biases = [
     tf.Variable(tf.zeros(2))]
 
 # Input
-features = tf.Variable([
-    [1.0, 2.0, 3.0, 4.0], 
-    [-1.0, -2.0, -3.0, -4.0], 
-    [11.0, 12.0, 13.0, 14.0]])
+features = tf.Variable([[0.0, 2.0, 3.0, 4.0], 
+                        [0.1, 0.2, 0.3, 0.4], 
+                        [11.0, 12.0, 13.0, 14.0]])
 
-# TODO: Create Model
+# Create Model
 # Features is 3x4
 # Hidden is 4x3
 #   That yields a hidden outut of 3x4 X 4x3 => 3x3, call it T
@@ -35,13 +36,14 @@ features = tf.Variable([
 
 hidden_output = tf.add(tf.matmul(features, weights[0]), biases[0])
 hidden_output = tf.nn.relu(hidden_output)
+hidden_output = tf.nn.dropout(hidden_output, 0.5)
 
-final_output = tf.add(tf.matmul(hidden_output, weights[1]), biases[1])
+logits = tf.add(tf.matmul(hidden_output, weights[1]), biases[1])
 
 
 
 # TODO: Print session results
 with tf.Session() as session:
     session.run(tf.global_variables_initializer())
-    print(session.run(final_output))
+    print(session.run(logits))
     
