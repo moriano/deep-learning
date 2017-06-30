6b9a2fe674fb383ca68868f0d51d1f03b5e47f01
moriano <moriano@gmail.com>
2017-07-01 09:08:58 +1000

    Convolution minisample

---
+++ introTensorFlow/convolutionTransformExample.py
@@ -0,0 +1,53 @@
+"""
+Setup the strides, padding and filter weight/bias such that
+the output shape is (1, 2, 2, 3).
+"""
+import tensorflow as tf
+import numpy as np
+
+# `tf.nn.conv2d` requires the input be 4D (batch_size, height, width, depth)
+# (1, 4, 4, 1)
+x = np.array([
+    [0, 1, 0.5, 10],
+    [2, 2.5, 1, -8],
+    [4, 0, 5, 6],
+    [15, 1, 2, 3]], dtype=np.float32).reshape((1, 4, 4, 1))
+X = tf.constant(x)
+
+"""
+Explanation
+So we want to go from an image that is
+    4x4x1
+To an image that is deeper and shrinked, that is
+    2x2x3
+    
+With SAME padding this is simple, we just put a patch of 2x2 and we make 
+strides of two pixels, that means that the stride can only be in two positions
+left and right, up and down, giving us 2x2
+
+Remember that the output when using SAME padding is 
+
+output = ceil(input/strides)
+
+That applies for both height and width
+"""
+def conv2d(input):
+    # Filter (weights and bias)
+    # The shape of the filter weight is (height, width, input_depth, output_depth)
+    # The shape of the filter bias is (output_depth,)
+    # TODO: Define the filter weights `F_W` and filter bias `F_b`.
+    # NOTE: Remember to wrap them in `tf.Variable`, they are trainable parameters after all.
+    F_W = tf.Variable(tf.random_normal([2, 2, 1, 3]))
+    F_b = tf.Variable(tf.random_normal([3]))
+    # TODO: Set the stride for each dimension (batch_size, height, width, depth)
+    strides = [1, 2, 2, 1]
+    # TODO: set the padding, either 'VALID' or 'SAME'.
+    padding = 'SAME'
+    # https://www.tensorflow.org/versions/r0.11/api_docs/python/nn.html#conv2d
+    # `tf.nn.conv2d` does not include the bias computation so we have to add it ourselves after.
+    return tf.nn.conv2d(input, F_W, strides, padding) + F_b
+
+out = conv2d(X)
+
+
+
